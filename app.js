817ea471c15e18547c3fd989eb8a1b60eac9b168
Rio Kivell <rjckivel@gmail.com>
2018-02-11 21:10:45 -0500

    Single post view!

--- app.js
+++ app.js
@@ -52,6 +52,19 @@ function($scope, postFactory){
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
   };
+  
+  $scope.addComment = function(){
+  if($scope.body === '') {
+    return;
+  }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+};
+
 }])
 .controller('PostsCtrl', [
 '$scope',
